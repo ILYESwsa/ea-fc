778b8399e0c0350a5212fd29a2a35abd48ab8a8f
ILYEScc <jhgn671@gmail.com>
2026-03-03 18:09:48 +0100

    Delete shared directory

--- shared/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './models/world.js';
-export * from './models/player.js';
-export * from './models/game.js';
-export * from './protocol/messages.js';
-export * from './utils/crafting.js';
